5a0fc350bd7e8e5eca0502d505a5676a01faf31f
WangAnChuan <wanganchuan060@gmail.com>
2025-12-23 02:16:21 +0800

    fix: 完善Vite代理配置，添加uploads路径转发

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -11,6 +11,16 @@ export default defineConfig({
         }
     },
     server: {
-        port: 3000
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:9095',
+                changeOrigin: true
+            },
+            '/uploads': {
+                target: 'http://localhost:9095',
+                changeOrigin: true
+            }
+        }
     }
 })
